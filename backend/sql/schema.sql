0f4dd3f3cde90fd4371f3565605374982e699804
Jose Chavez <josmchavz@gmail.com>
2024-02-20 09:33:28 -0800

    fixed minor bugs to startup the server

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,3 +1,3 @@
-DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE user(id SERIAL PRIMARY KEY, username VARCHAR(50), email VARCHAR(50), profilepic VARCHAR(50), password VARCHAR(50), role VARCHAR(50), state VARCHAR(50), created_at TIMESTAMP, updated_at TIMESTAMP);
+CREATE TABLE users(id SERIAL PRIMARY KEY, username VARCHAR(50), email VARCHAR(50), profilepic VARCHAR(50), password VARCHAR(50), role VARCHAR(50), state VARCHAR(50), created_at TIMESTAMP, updated_at TIMESTAMP);
